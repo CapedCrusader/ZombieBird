69e53ddb4a5f0d5254af567b6e18012940ec6117
CapedCrusader <CapedCrusader@users.noreply.github.com>
2015-01-09 18:06:43 +0530

    Adding final files

---
+++ ZombieBird/src/com/kilobolt/ZBHelpers/InputHandler.java
@@ -0,0 +1,78 @@
+package com.kilobolt.ZBHelpers;
+
+import com.badlogic.gdx.InputProcessor;
+import com.kilobolt.GameObjects.Bird;
+import com.kilobolt.GameWorld.GameWorld;
+
+public class InputHandler implements InputProcessor{
+	
+	private Bird myBird;
+	private GameWorld myWorld;
+	
+	// Ask for a reference to the Bird when InputHandler is created.
+    public InputHandler(GameWorld myWorld) {
+        // myBird now represents the gameWorld's bird.
+        this.myWorld = myWorld;
+        myBird = myWorld.getBird();
+    }
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+
+        if (myWorld.isReady()) {
+            myWorld.start();
+        }
+
+        myBird.onClick();
+
+        if (myWorld.isGameOver() || myWorld.isHighScore()) {
+            // Reset all variables, go to GameState.READ
+            myWorld.restart();
+        }
+
+        return true;
+    }
+
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchMoved(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
